09cc6903d61c45ed04038a293ff37a9179d0cb44
zimbatm <zimbatm@oree.ch>
2009-05-26 09:31:20 +0200

    Small doc improvements

--- example/patch_usage.rb
+++ example/patch_usage.rb
@@ -1,16 +1,16 @@
 require 'monkeypatch'
 
 # Define a new extension that adds the #to_blob method
-date_patch = MonkeyPatch.add_method(:to_blob) do
+blob_patch = MonkeyPatch.add_method(:to_blob) do
   def to_blob; "<blob>" end
 end
 
 x = "something"
-date_patch.patch_instance(x)
+blob_patch.patch_instance(x)
 x.to_blob #=> "<blob>"
 
 # Define a patch, that replaces the #to_date method
-each_patch = MonkeyPatch.replace_method(:to_s) do
+str_patch = MonkeyPatch.replace_method(:to_s) do
   def to_s; "..." end
 end
 
@@ -18,7 +18,8 @@ class ExampleClass
   def to_s; "hello" end
 end
 
-(date_patch & each_patch).patch_class(ExampleClass)
+(blob_patch & str_patch).patch_class(ExampleClass)
 
 ExampleClass.new.to_s #=> "..."
 ExampleClass.new.to_blob #=> "<blob>"
+
